dec88974d6b390aefba3bc063d352411ff2d5b53
Sean Saunders <saunders.m.sean@gmail.com>
2023-10-05 22:26:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function moveDodgerLeft() {
+    let left = parseInt(dodger.style.left);
+    dodger.style.left = `${left - 10}px`;
+  }
+  function moveDodgerRight() {
+    let left = parseInt(dodger.style.left);
+    dodger.style.left = `${left + 10}px`; 
+  }
